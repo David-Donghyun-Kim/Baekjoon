66d2f06233ac4453f979b08005b6c3fbcf264bb5
dhdavidkim1204@gmail.com <dhdavidkim1204@gmail.com>
2020-07-16 16:44:36 +0900

    백준

---
+++ 10. 수학2/[1929]소수 구하기.cpp
@@ -0,0 +1,21 @@
+#include<iostream>
+int main()
+{
+    int M,N;
+    std::cin>>M>>N;
+    int *arr = new int[N+1]{0,1}; // 0이면 소수, 1이면 소수 아님
+    for(int i=2;i<N+1;i++)
+    {
+        if(!(arr[i])) //arr[i] == 0
+        {
+            if(i * i > 1000000) break; // 문제에서 주어진 범위가 1,000,000임
+            for(int j = i*i;j<N+1;j+=i)
+                arr[j] = 1;
+        }
+    }
+    for(int i=M;i<N+1;i++)
+    {
+        if(!(arr[i])) std::cout<<i<<"\n";
+    }
+    return 0;
+}
