1f78b6210a5c2769e781b76984420ab8a1960c2d
dhdavidkim1204@gmail.com <dhdavidkim1204@gmail.com>
2020-07-24 12:23:29 +0900

    백준

---
+++ 13. 브루트 포스/[2798]블랙잭.cpp
@@ -0,0 +1,23 @@
+#include <iostream>
+#define MAX 100
+int main()
+{
+    int N, M, max = 0;
+    std::cin >> N >> M;
+    int arr[MAX];
+    for (int i = 0; i < N; i++)
+        std::cin >> arr[i];
+    for (int i = 0; i < N - 2; i++)
+    {
+        for (int j = i + 1; j < N - 1; j++)
+        {
+            for (int k = j + 1; k < N; k++)
+            {
+                int sum = arr[i] + arr[j] + arr[k];
+                if (max < sum && sum <= M)
+                    max = sum;
+            }
+        }
+    }
+    std::cout << max << "\n";
+}
